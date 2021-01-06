8ff004f00cfbd274507ad7352075b571a990d0bb
JoseIgnacioTapia <josignaciot@gmail.com>
2021-01-06 18:21:13 -0300

    Initial logic game

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+const btnRoll = document.querySelector('.btn--roll');
+const dice = document.querySelector('.dice');
+
+let number;
+let currentScore = 0;
+let activePlayer = 1;
+
+// Switch Player
+const switchPlayer = function() {
+
+}
+
+// Show dice
+function showDice() {
+  number = Math.floor(Math.random() * 6) + 1;
+  dice.setAttribute('src', `./img/dice-${number}.png`);
+  dice.style.display = 'block';
+  if (number === 1) {
+    switchPlayer();
+  } else {
+    currentScore += number;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  }
+}
+
+// Event Listeners
+btnRoll.addEventListener('click', showDice);
